3e077c498abdc48d13f2d79a85e0b9b1b0e7d8ba
Josh Charles <josh.charles@gmail.com>
2017-01-02 18:08:49 -0800

    Implemented CommandHydrator

---
+++ Apollo/CommandSystem/CommandProcessor.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Apollo.CommandSystem
+{
+    public class CommandProcessor : ICommandProcessor
+    {
+        private readonly ICommandHydrator hydrator;
+
+        public CommandProcessor(ICommandHydrator hydrator)
+        {
+            this.hydrator = hydrator;
+        }
+
+        public async Task<CommandResult> Process(ICommand command, object parameters)
+        {
+            var stopWatch = Stopwatch.StartNew();
+
+            hydrator.Hydrate(ref command, parameters);
+
+            var result = await ValidateCommand(command);
+
+            if(result == null)
+                result = await AuthorizeCommand(command);
+
+            if(result == null)
+                result = await ExecuteCommand(command);
+
+            result.Elapsed = stopWatch.ElapsedMilliseconds;
+            return result;
+        }
+
+        private async Task<CommandResult> ExecuteCommand(ICommand command)
+        {
+            try
+            {
+                var result = await command.Execute();
+                return result;
+            }
+            catch (Exception exception)
+            {
+                return new CommandResult
+                {
+                    ErrorMessage = exception.Message,
+                    Result = new {command, exception},
+                    ResultStatus = CommandResultType.Error
+                };
+            }
+        }
+
+        private async Task<CommandResult> AuthorizeCommand(ICommand command)
+        {
+            var result = new CommandResult();
+            try
+            {
+                if (!await command.Authorize())
+                {
+                    result.ErrorMessage = "Unauthorized Command";
+                    result.ResultStatus = CommandResultType.Unauthorized;
+                    result.Result = command;
+                    return result;
+                }
+
+                return null;
+            }
+            catch (Exception exception)
+            {
+                result.ErrorMessage = exception.Message;
+                result.ResultStatus = CommandResultType.Unauthorized;
+                result.Result = new {command, exception};
+            }
+
+            return result;
+        }
+
+        private async Task<CommandResult> ValidateCommand(ICommand command)
+        {
+            var result = new CommandResult();
+            try
+            {
+                if (!await command.IsValid())
+                {
+                    result.ErrorMessage = "Invalid Command";
+                    result.ResultStatus = CommandResultType.Invalid;
+                    result.Result = command;
+                    return result;
+                }
+
+                return null;
+            }
+            catch (Exception exception)
+            {
+                result.ErrorMessage = exception.Message;
+                result.ResultStatus = CommandResultType.Invalid;
+                result.Result = new {command, exception};
+            }
+
+            return result;
+        }
+    }
+}
--- Apollo/CommandSystem/ICommandLocator.cs
+++ Apollo/CommandSystem/ICommandLocator.cs
@@ -1,34 +1,7 @@
-﻿using System.Linq;
-using Apollo.ServiceLocator;
-
-namespace Apollo.CommandSystem
+﻿namespace Apollo.CommandSystem
 {
     public interface ICommandLocator
     {
         ICommand Locate(string commandName);
-    }
-
-    public class CommandLocator : ICommandLocator
-    {
-        private readonly IServiceLocator serviceLocator;
-
-        public CommandLocator(IServiceLocator serviceLocator)
-        {
-            this.serviceLocator = serviceLocator;
-        }
-
-        public ICommand Locate(string commandName)
-        {
-            commandName = commandName.ToLowerInvariant();
-            var type = typeof(ICommand);
-            var commandType = type.Assembly.GetTypes()
-                .Where(p => type.IsAssignableFrom(p))
-                .SingleOrDefault(t => commandName == t.Name.ToLowerInvariant());
-
-            if (commandType == null)
-                return null;
-
-            return (ICommand)serviceLocator.Get(commandType);
-        }
     }
 }
