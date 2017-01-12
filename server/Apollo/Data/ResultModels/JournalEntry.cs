552b3b9346507a88f57b1c2059434489861c55b9
Josh Charles <josh.charles@gmail.com>
2017-01-11 20:33:51 -0800

    Querying the database from the server.  BAM

---
+++ server/Apollo/Data/ResultModels/JournalEntry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Apollo.Data.ResultModels
+{
+    public class JournalEntry
+    {
+        public int id { get; set; }
+        public string note { get; set; }
+        public DateTime created_at { get; set; }
+    }
+}
