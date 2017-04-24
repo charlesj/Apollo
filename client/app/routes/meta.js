bd8dcc63a767d5dfddc2b0ed32c4bf49708df29b
Josh Charles <josh.charles@gmail.com>
2017-04-23 18:11:02 -0700

    Initial React App from CLI

--- client/app/routes/meta.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  apollo: Ember.inject.service('apollo-server'),
-  model: function(){
-    return this.get('apollo').request('ApplicationInfo', {});
-  }
-});
