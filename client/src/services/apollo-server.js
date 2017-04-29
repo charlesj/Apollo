b6d1a0de6a16ac4e18bb27742ba51df8b9625ce4
Josh Charles <josh.charles@gmail.com>
2017-04-29 12:09:01 -0700

    Simple improvements to apollo-serve

--- client/src/services/apollo-server.js
+++ client/src/services/apollo-server.js
@@ -1,21 +1,20 @@
 var axios = require('axios');
 var config = require('../config');
 
+var requestCounter = 0;
+
 module.exports = {
   invoke: function(commandName, payload) {
-    console.log(`COMMAND ${commandName}`);
-    console.log(payload);
+    console.log(`COMMAND ${commandName}`, payload);
     return axios.post(config.apiUrl + 'api', {
-      id: 'whatever',
+      id: (requestCounter++).toString(),
       method: commandName,
       params: payload
     }).then(response => {
-      console.log("RESPONSE SUCCESS");
-      console.log(response);
+      console.log("RESPONSE SUCCESS", response);
       return response.data.result.Result;
     }).catch(err => {
-      console.log("ERROR");
-      console.log(err);
+      console.log("ERROR", err);
     });
   }
 };
