b819388f7d0a876ab505d20034e4670178b8abb3
Josh Charles <josh.charles@gmail.com>
2017-04-29 22:10:22 -0700

    Adding Base64 Converter wrapper

---
+++ server/Apollo.Tests/Utilities/Base64ConverterTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using Apollo.Utilities;
+using Xunit;
+
+namespace Apollo.Tests.Utilities
+{
+    public class Base64ConverterTests : BaseUnitTest<Base64Converter>
+    {
+        private string messageEncoded = "VGhpcyBpcyBteSBtZXNzYWdl";
+        private byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+        const string message = "This is my message";
+
+        [Fact]
+        public void CanGetEncodingFromBytes()
+        {
+            var actual = this.ClassUnderTest.Encode(messageBytes);
+            Assert.Equal(messageEncoded, actual);
+        }
+
+        [Fact]
+        public void CanGetEncodingFromStringAssumingUTF8()
+        {
+            var actual = this.ClassUnderTest.Encode(message);
+            Assert.Equal(messageEncoded, actual);
+        }
+
+        [Fact]
+        public void CanGetBytesFromBase64String()
+        {
+            var actual = this.ClassUnderTest.Decode(messageEncoded);
+            Assert.Equal(messageBytes, actual);
+        }
+
+        [Fact]
+        public void CanGetStringFromBase64String()
+        {
+            var actual = this.ClassUnderTest.DecodeString(messageEncoded);
+            Assert.Equal(message, actual);
+        }
+    }
+}
---
+++ server/Apollo/Utilities/Base64Converter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace Apollo.Utilities
+{
+    public interface IBase64Converter
+    {
+        byte[] Decode(string encoded);
+        string DecodeString(string encoded);
+        string Encode(byte[] bytes);
+        string Encode(string message);
+    }
+
+    public class Base64Converter : IBase64Converter
+    {
+        public byte[] Decode(string encoded)
+        {
+            return Convert.FromBase64String(encoded);
+        }
+
+        public string DecodeString(string encoded)
+        {
+            var bytes = this.Decode(encoded);
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+
+        public string Encode(byte[] bytes)
+        {
+            return Convert.ToBase64String(bytes);
+        }
+
+        public string Encode(string message)
+        {
+            var bytes = Encoding.UTF8.GetBytes(message);
+            return Encode(bytes);
+        }
+    }
+}
