863d4230d202fd5cbc784741f0febc36d95536a0
Josh Charles <josh.charles@gmail.com>
2017-06-20 08:32:55 -0700

    Browser plugin is fully functional, but needs polish.

--- browser_plugin/popup.js
+++ browser_plugin/popup.js
@@ -8,6 +8,10 @@
  * @param {function(string)} callback - called when the URL of the current tab
  *   is found.
  */
+
+var endpoint = "https://apollo.sudolife.org/api";
+var requestCounter = 0;
+
 function getCurrentTabUrl(callback) {
   // Query filter to be passed to chrome.tabs.query - see
   // https://developer.chrome.com/extensions/tabs#method-query
@@ -47,14 +51,135 @@ function getCurrentTabUrl(callback) {
   // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
+function toggleDisplay(elementId, show){
+    var element = document.getElementById(elementId);
+    if(show === true ){
+        element.style.display = 'block';
+    } else {
+        element.style.display = 'none';
+    }
+}
+
+function checkLogin(){
+    chrome.storage.sync.get('token', function(storage){
+       if(storage.token){
+           toggleDisplay('addBookmarkForm', true);
+           toggleDisplay('loginForm', false);
+       }else{
+           toggleDisplay('addBookmarkForm', false);
+           toggleDisplay('loginForm', true);
+       }
+    });
+}
+
+function getToken(callback){
+    chrome.storage.sync.get('token', function(storage){
+        callback(storage.token);
+    });
+}
+
+function setToken(token, callback){
+    chrome.storage.sync.set({'token': token}, function(){
+        printToken();
+        callback();
+    });
+}
+
+function clearToken(callback){
+    chrome.storage.sync.remove('token', callback);
+}
+
+function printToken(){
+    getToken(function(token){
+       console.log('token :', token);
+    });
+}
+
+function login(){
+    var password = document.getElementById('password').value;
+    apolloRPC('login', {
+        password: password
+    }, function(result){
+        if(result.token){
+            console.log('logging in');
+            setToken(result.token, function(){
+                checkLogin();
+            });
+        }
+    })
+}
+
+function apolloRPC(command, payload, callback){
+    getToken(function(token){
+        payload.token = token;
+        console.log(payload);
+        var ajaxRequest = new XMLHttpRequest();
+        ajaxRequest.open("POST", endpoint);
+        ajaxRequest.setRequestHeader("Content-Type", "application/json");
+        ajaxRequest.onreadystatechange = function() {
+            if (ajaxRequest.readyState == 4) {
+                console.log('Received Respnose', ajaxRequest);
+                var resp = JSON.parse(ajaxRequest.responseText);
+                callback(resp.result.Result);
+            }
+        }
+        ajaxRequest.send(JSON.stringify({id: 'bk' + (requestCounter++).toString(), params: payload, method: command}));
+    })
+}
+
+function logout(){
+    getToken(function(token){
+        apolloRPC('revokeLoginSession', {
+            tokenToRevoke: token
+        }, function(result){
+            console.log('logging out');
+            clearToken(function(){
+               printToken();
+               checkLogin();
+            });
+        });
+    });
+}
+
+function submitBookmark(){
+    var link = document.getElementById('pageUrl').value;
+    var title = document.getElementById('pageTitle').value;
+    var description = document.getElementById('description').value;
+    var rawTags = document.getElementById('tags').value;
+    var tags = [];
+    if(rawTags){
+        tags = rawTags.split(',');
+    }
+
+    apolloRPC('addbookmark',{
+        title: title,
+        link: link,
+        description: description,
+        tags: tags
+    }, function(){
+        toggleDisplay('success', true);
+    });
+}
+
+function showLoading(){
+    toggleDisplay('loading', true);
+}
+
+function hideLoading(){
+    toggleDisplay('loading', false);
+}
 
 document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url, title) {
+  document.getElementById("logout").addEventListener("click", logout);
+  document.getElementById("login").addEventListener("click", login);
+  document.getElementById("submitBookmark").addEventListener("click", submitBookmark);
+});
 
+document.addEventListener('DOMContentLoaded', function() {
+  toggleDisplay('success', false);
+  checkLogin();
+  getCurrentTabUrl(function(url, title) {
     document.getElementById('pageUrl').value = url;
     document.getElementById('pageTitle').value = title;
-
-
-
   });
 });
