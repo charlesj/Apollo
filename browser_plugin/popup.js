c369e6741d7d56e5633363ec6a41a5acd62f9a87
Josh Charles <josh.charles@gmail.com>
2017-06-21 20:34:57 -0700

    Browser plugin will display if page is already bookmarked

--- browser_plugin/popup.js
+++ browser_plugin/popup.js
@@ -13,42 +13,22 @@ var endpoint = "https://apollo.sudolife.org/api";
 var requestCounter = 0;
 
 function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
   var queryInfo = {
     active: true,
     currentWindow: true
   };
 
   chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
     var tab = tabs[0];
 
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
     var url = tab.url;
     var title = tab.title;
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
+
     console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url, title);
+    checkForBookmark(url, function(bookmarks){
+        callback(url, title, bookmarks);
+    });
   });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
 }
 
 function toggleDisplay(elementId, show){
@@ -107,6 +87,12 @@ function login(){
             });
         }
     })
+}
+
+function checkForBookmark(url, callback){
+    apolloRPC('getBookmarks', {link: url}, function(result){
+        callback(result.bookmarks);
+    });
 }
 
 function apolloRPC(command, payload, callback){
@@ -180,8 +166,15 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
   toggleDisplay('success', false);
   checkLogin();
-  getCurrentTabUrl(function(url, title) {
+  getCurrentTabUrl(function(url, title, bookmarks) {
     document.getElementById('pageUrl').value = url;
     document.getElementById('pageTitle').value = title;
+    if(bookmarks.length > 0){
+        toggleDisplay('alreadyMarked', true);
+        document.getElementById('alreadyMarked').innerHTML="Bookmarked " + bookmarks[0].created_at;
+    }else{
+        toggleDisplay('alreadyMarked', false);
+    }
+
   });
 });
