14a8b25620c85160fe22eed42e7ab1574114bba0
Josh Charles <josh.charles@gmail.com>
2017-07-07 10:33:12 -0700

    Tweaking navigation

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -5,16 +5,12 @@ import FontAwesome from 'react-fontawesome';
 function Nav() {
   return (
     <nav>
-        <div>
-            <div>
-            <NavLink exact activeClassName='active' to='/'><FontAwesome name='home' size='2x' /></NavLink>
-            <NavLink activeClassName='active' to='/todo'><FontAwesome name='check-square-o' size='2x' /></NavLink>
-            <NavLink activeClassName='active' to='/journal'><FontAwesome name='pencil' size='2x' /></NavLink>
-            <NavLink activeClassName='active' to='/health'><FontAwesome name='medkit' size='2x' /></NavLink>
-            <NavLink activeClassName='active' to='/bookmarks'><FontAwesome name='bookmark-o' size='2x' /></NavLink>
-            <NavLink activeClassName='active' to='/settings'><FontAwesome name='gear' size='2x' /></NavLink>
-            </div>
-        </div>
+      <NavLink exact activeClassName='active' to='/'><FontAwesome name='home' size='1x' /> Home</NavLink>
+      <NavLink activeClassName='active' to='/todo'><FontAwesome name='check-square-o' size='1x' /> Organize</NavLink>
+      <NavLink activeClassName='active' to='/journal'><FontAwesome name='pencil' size='1x' /> Log</NavLink>
+      <NavLink activeClassName='active' to='/health'><FontAwesome name='medkit' size='1x' /> Health</NavLink>
+      <NavLink activeClassName='active' to='/bookmarks'><FontAwesome name='bookmark-o' size='1x' /> Bookmarks</NavLink>
+      <NavLink activeClassName='active' to='/settings'><FontAwesome name='gear' size='1x' /> Settings</NavLink>
     </nav>
   )
 }
