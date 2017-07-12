767a73428d021e75e155bb4dba7e24b9676d9c31
Josh Charles <josh.charles@gmail.com>
2017-07-12 10:10:01 -0700

    Ported Apollo.Common to dotnet core

---
+++ server/Apollo/Utilities/Clock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Apollo.Utilities
+{
+    public interface IClock
+    {
+        DateTimeOffset UtcNow { get; }
+        Task Delay(int milliseconds);
+    }
+
+    public class Clock : IClock
+    {
+        public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
+
+        public async Task Delay(int milliseconds)
+        {
+            await Task.Delay(milliseconds);
+        }
+    }
+}
---
+++ server/Apollo/Utilities/ConsoleNotShitty.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Apollo.Utilities
+{
+    public class ConsoleNotShitty
+    {
+        public void Write(string output, bool newLine = true, ConsoleColor color = ConsoleColor.Gray)
+        {
+            var currentConsoleColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            if (newLine)
+                Console.WriteLine(output);
+            else
+                Console.Write(output);
+
+            Console.ForegroundColor = currentConsoleColor;
+        }
+
+        public void Red(string output, bool newLine = true)
+        {
+            Write(output, newLine, ConsoleColor.Red);
+        }
+
+        public void Yellow(string output, bool newLine = true)
+        {
+            Write(output, newLine, ConsoleColor.Yellow);
+        }
+
+        public void Green(string output, bool newLine = true)
+        {
+            Write(output, newLine, ConsoleColor.Green);
+        }
+
+        public string ReadLineSupressOutput()
+        {
+            var line = new Stack<char>();
+            var key = Console.ReadKey(true);
+            while (key.Key != ConsoleKey.Enter)
+            {
+                if (key.Key == ConsoleKey.Backspace)
+                {
+                    if(line.Count > 0)
+                        line.Pop();
+                    continue;
+                }
+
+                line.Push(key.KeyChar);
+                key = Console.ReadKey(true);
+            }
+
+            Console.WriteLine(string.Empty);
+
+            return new string(line.Reverse().ToArray());
+        }
+    }
+}
