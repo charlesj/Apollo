40bd2de48da9a0af18b6f170f073c08588c89801
Josh Charles <josh.charles@gmail.com>
2017-07-15 10:55:37 -0700

    Fixing small client error

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -5,12 +5,12 @@ import FontAwesome from 'react-fontawesome';
 function Nav() {
   return (
     <nav>
-      <NavLink exact activeClassName='active' to='/'><FontAwesome name='home' size='1x' /> Home</NavLink>
-      <NavLink activeClassName='active' to='/todo'><FontAwesome name='check-square-o' size='1x' /> Organize</NavLink>
-      <NavLink activeClassName='active' to='/journal'><FontAwesome name='pencil' size='1x' /> Log</NavLink>
-      <NavLink activeClassName='active' to='/health'><FontAwesome name='medkit' size='1x' /> Health</NavLink>
-      <NavLink activeClassName='active' to='/bookmarks'><FontAwesome name='bookmark-o' size='1x' /> Bookmarks</NavLink>
-      <NavLink activeClassName='active' to='/settings'><FontAwesome name='gear' size='1x' /> Settings</NavLink>
+      <NavLink exact activeClassName='active' to='/'><FontAwesome name='home' /> Home</NavLink>
+      <NavLink activeClassName='active' to='/todo'><FontAwesome name='check-square-o' /> Organize</NavLink>
+      <NavLink activeClassName='active' to='/journal'><FontAwesome name='pencil' /> Log</NavLink>
+      <NavLink activeClassName='active' to='/health'><FontAwesome name='medkit' /> Health</NavLink>
+      <NavLink activeClassName='active' to='/bookmarks'><FontAwesome name='bookmark-o' /> Bookmarks</NavLink>
+      <NavLink activeClassName='active' to='/settings'><FontAwesome name='gear' /> Settings</NavLink>
     </nav>
   )
 }
