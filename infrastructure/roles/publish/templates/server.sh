56bb820c0d6a6ef837e459dee7543f40af567fb4
Josh Charles <josh.charles@gmail.com>
2017-07-15 17:50:31 -0700

    Changing server systemd script to reflect dotnet core

--- infrastructure/roles/publish/templates/server.sh
+++ infrastructure/roles/publish/templates/server.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-APOLLO_EXE=/var/apollo/server/Apollo.exe
+APOLLO_EXE=/var/apollo/server/Apollo.dll
 
 # database configuration
-APOLLO_DATABASENAME='apollo_db'
+APOLLO_DATABASENAME='{{ dbname }}'
 APOLLO_DATABASESERVER='127.0.0.1'
-APOLLO_DATABASEUSERNAME='apollo_pg'
-APOLLO_DATABASEPASSWORD='apollo_db_password'
+APOLLO_DATABASEUSERNAME='{{ dbuser }}'
+APOLLO_DATABASEPASSWORD='{{ dbpassword }}'
 
 export APOLLO_DATABASENAME
 export APOLLO_DATABASESERVER
 export APOLLO_DATABASEUSERNAME
 export APOLLO_DATABASEPASSWORD
 
-mono "$APOLLO_EXE"
+dotnet "$APOLLO_EXE"
