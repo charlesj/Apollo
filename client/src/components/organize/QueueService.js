369a2af925a092c7b170f1665068fcc31c7261ed
Josh Charles <josh.charles@gmail.com>
2017-07-29 12:29:15 -0700

    Updating client to talk to server

--- client/src/components/organize/QueueService.js
+++ client/src/components/organize/QueueService.js
@@ -1,65 +1,15 @@
-import store from 'store';
-
-var dataKey = "queueItems";
+import apolloServer from '../../services/apollo-server';
 
 function getQueueItems(){
-  var data = store.get(dataKey);
-  if(data == null){
-    store.set(dataKey, [
-      {
-        id: 1,
-        title: "here is title",
-        link: "http://cnn.com",
-        description: "here is my description",
-        created_at: new Date(),
-        completed_at: null,
-      },
-      {
-        id: 2,
-        title: "Second Queue Item",
-        link: "http://example.com",
-        description: "no real description",
-        created_at: new Date(),
-        completed_at: null,
-      },
-    ]);
-    data = store.get(dataKey);
-  };
-
-  return new Promise((resolve) => {
-    resolve(data.filter(d => {
-      return d.completed_at === null;
-    }));
-  });
+  return apolloServer.invoke("getTodoQueueItems", {});
 }
 
 function updateItem(item){
-  var currentItems = store.get(dataKey);
-  var newItems = [];
-  currentItems.forEach(i => {
-    if(i.id === item.id){
-      newItems.push(item);
-    } else{
-      newItems.push(i);
-    }
-  });
-
-  store.set(dataKey, newItems);
-
-  return new Promise((resolve) => {
-    resolve();
-  });
+  return apolloServer.invoke("updateTodoQueueItem", {item});
 }
 
 function addItem(item){
-  var currentItems = store.get(dataKey);
-  item.id = currentItems.length + 1;
-  currentItems.push(item);
-  store.set(dataKey, currentItems);
-  console.log(currentItems);
-  return new Promise((resolve) => {
-    resolve();
-  });
+  return apolloServer.invoke("addTodoQueueItem", {item});
 }
 
 module.exports = {
