9ec436339d5e74b59d723b476f764f86df1bc832
Josh Charles <josh.charles@gmail.com>
2017-07-29 22:18:06 -0700

    Simple Schedule service

---
+++ server/Apollo.Tests/Services/ScheduleServiceTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using Apollo.Services;
+using Xunit;
+
+namespace Apollo.Tests.Services
+{
+    public class ScheduleServiceTests : BaseUnitTest<SchedulerService>
+    {
+        private DateTime beginTime = new DateTime(1983, 2, 13, 4, 2, 0);
+
+        public class GenerateNextEvents : ScheduleServiceTests
+        {
+            [Fact]
+            public void CanGenerateHourlySchedule()
+            {
+                var schedule = new Schedule {hourly = true, start = beginTime};
+                var next = ClassUnderTest.GenerateEvents(schedule).Take(3);
+                Assert.Collection(next,
+                    e => Assert.Equal(beginTime.AddHours(1), e),
+                    e => Assert.Equal(beginTime.AddHours(2), e),
+                    e => Assert.Equal(beginTime.AddHours(3), e));
+            }
+
+            [Fact]
+            public void RepeatCountRespected()
+            {
+                var schedule = new Schedule {daily = true, repeat_count = 1, start = beginTime};
+                var next = ClassUnderTest.GenerateEvents(schedule).Take(3);
+                Assert.Collection(next,
+                    e => Assert.Equal(beginTime.AddDays(1), e));
+            }
+        }
+
+        public class GenerateInterval : ScheduleServiceTests
+        {
+            [Fact]
+            public void HourTimespan()
+            {
+                var schedule = new Schedule {hourly = true};
+                var interval = ClassUnderTest.GenerateInterval(schedule);
+                Assert.Equal(TimeSpan.FromHours(1), interval);
+            }
+
+            [Fact]
+            public void DailyTimespan()
+            {
+                var schedule = new Schedule {daily = true};
+                var interval = ClassUnderTest.GenerateInterval(schedule);
+                Assert.Equal(TimeSpan.FromDays(1), interval);
+            }
+        }
+    }
+}
---
+++ server/Apollo/Services/SchedulerService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Apollo.Services
+{
+    public interface ISchedulerService
+    {
+        IEnumerable<DateTimeOffset> GenerateEvents(Schedule schedule, DateTimeOffset start);
+    }
+
+    public class SchedulerService
+    {
+        public IEnumerable<DateTimeOffset> GenerateEvents(Schedule schedule)
+        {
+            var curr = schedule.start;
+            var generatedCount = 0;
+            var interval = GenerateInterval(schedule);
+            while (schedule.repeat_count == null || generatedCount++ < schedule.repeat_count.Value)
+            {
+                curr = curr.Add(interval);
+                yield return curr;
+            }
+        }
+
+        public TimeSpan GenerateInterval(Schedule schedule)
+        {
+            if (schedule.daily)
+            {
+                return TimeSpan.FromDays(1);
+            }
+
+            if (schedule.hourly)
+            {
+                return TimeSpan.FromHours(1);
+            }
+
+            return TimeSpan.MaxValue;
+        }
+    }
+
+    public class Schedule
+    {
+        public DateTime start;
+        public bool hourly { get; set; }
+        public bool daily { get; set; }
+        public int? repeat_count { get; set; }
+    }
+}
