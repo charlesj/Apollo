50b628f10ec83a73a5a71b682816b5c4c6765fb3
Josh Charles <josh.charles@gmail.com>
2017-09-18 23:58:49 -0700

    Basic Client Implementation that will require much more work.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HomeHealth from './health/HomeHealth';
 import Summary from './Summary';
+import Weather from './weather/Weather';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
         <Summary />
+        <Weather />
         <hr />
         <HomeHealth />
       </div>
---
+++ client/src/components/weather/Weather.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import moment from 'moment';
+import apolloServer from '../../services/apollo-server';
+import WeatherIcon from './WeatherIcon';
+
+var DarkSkyIconMapping = {
+  "clear-day": "wi-day-sunny",
+  "clear-night":"wi-night-clear",
+  "rain":"wi-day-rain",
+  "snow":"wi-day-snow",
+  "sleet":"wi-day-rain-mix",
+  "wind":"wi-day-cloudy-gusts",
+  "fog":"wi-day-fog",
+  "cloudy":"wi-day-cloudy",
+  "partly-cloudy-day":"wi-day-cloudy-high",
+  "partly-cloudy-night":"wi-night-cloudy-high",
+  "hail":"wi-day-hail",
+  "thunderstorm":"wi-day-lightning",
+  "tornado":"wi-tornado"
+}
+
+function ForecastDay(props){
+  console.log(props.f);
+  var date = moment.unix(props.f.time);
+  return (<div className='forecastDay'>
+    <div><WeatherIcon icon={DarkSkyIconMapping[props.f.icon]} /></div>
+    <div>{date.calendar()}</div>
+    <div>{props.f.summary}</div>
+    <div>H {props.f.temperatureHigh}&#8457;</div>
+    <div>L {props.f.temperatureLow}&#8457;</div>
+  </div>);
+}
+
+function ForecastDisplay(props){
+  return (<div className='weatherForecast'>
+    <div className='currentConditions'>
+    <div className='weatherIcon'>
+    <WeatherIcon icon={DarkSkyIconMapping[props.data.Forecast.currently.icon]} />
+    </div>
+    {props.data.Location} - {props.data.Forecast.currently.summary} <br />
+    {props.data.Forecast.currently.temperature}&#8457;
+    </div>
+    {props.data.Forecast.daily.data.map((d, i) => {
+      return <ForecastDay key={i} f={d} />
+    })}
+  </div>)
+}
+
+class Weather extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      data: null
+    };
+  }
+
+  componentDidMount(){
+    apolloServer.invoke('getWeatherForecasts', {})
+      .then(data => {
+        this.setState({data});
+      });
+  }
+
+  render(){
+    if(this.state.data === null){
+      return (<div className='weatherContainer'>Loading Weather...</div>);
+    }
+
+    return (<div className='weatherContainer'>
+      { this.state.data.forecasts.map((f, i) => {
+        return (<ForecastDisplay key={i} data={f} />)
+      }) }
+    </div>);
+  }
+}
+
+
+module.exports = Weather;
