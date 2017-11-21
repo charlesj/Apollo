664fec3fcad1c43fc8086f3a784523571bc64856
Josh Charles <josh.charles@gmail.com>
2017-11-21 14:01:56 -0800

    Adding Oblique Strategies

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,15 +2,16 @@ import React from 'react';
 import HomeHealth from './health/HomeHealth';
 import Summary from './Summary';
 import Weather from './weather/Weather';
+import ObliqueStrategies from './thinking/ObliqueStrategies';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <Summary />
         <Weather />
-        <hr />
+        <Summary />
         <HomeHealth />
+        <ObliqueStrategies />
       </div>
     )
   }
