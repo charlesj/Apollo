5b169a2bd400dcffb657f095249b16abbc4b4211
Josh Charles <josh.charles@gmail.com>
2017-12-30 13:42:04 -0800

    Adding complete checklist back to home

--- client/src/react/Home.js
+++ client/src/react/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Summaries from "./meta/Summaries";
 import Weather from "./weather/Weather";
 import Strategies from "./thinking/ObliqueStrategies";
+import CompleteChecklist from "./checklists/CompleteChecklist";
 
 function Home(props) {
   return (
     <div>
       <Weather />
       <Summaries />
+      <CompleteChecklist />
       <Strategies />
     </div>
   );
