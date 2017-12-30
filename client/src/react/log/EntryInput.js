311baf096d0690fe09de2a257b79c6abc09c53ce
Josh Charles <josh.charles@gmail.com>
2017-12-30 12:17:33 -0800

    Adding log back in

---
+++ client/src/react/log/EntryInput.js
@@ -0,0 +1,121 @@
+import _ from "lodash";
+import React from "react";
+import PropTypes from "prop-types";
+
+class EntryInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      note: "",
+      newTag: "",
+      tags: []
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleTagChange = this.handleTagChange.bind(this);
+    this.handleTagKeyPress = this.handleTagKeyPress.bind(this);
+    this.addTag = this.addTag.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.removeTag = this.removeTag.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      note: e.target.value
+    });
+  }
+
+  handleTagChange(event) {
+    this.setState({
+      newTag: event.target.value
+    });
+  }
+
+  handleTagKeyPress(target) {
+    if (target.charCode === 13) {
+      this.addTag();
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.note, this.state.tags);
+    this.setState({
+      note: "",
+      tags: []
+    });
+  }
+
+  addTag() {
+    var currentTags = this.state.tags;
+    currentTags.push(this.state.newTag);
+    this.setState({
+      newTag: "",
+      tags: currentTags
+    });
+  }
+
+  removeTag(tag) {
+    var currentTags = this.state.tags;
+    _.remove(currentTags, t => {
+      return t === tag;
+    });
+    this.setState({
+      newTag: "",
+      tags: currentTags
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="pt-card">
+          <div className="pt-form-group">
+            <textarea
+              placeholder="Add note"
+              value={this.state.note}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="pt-form-group">
+            <input
+              onChange={this.handleTagChange}
+              placeholder="Add tags"
+              value={this.state.newTag}
+              onKeyPress={this.handleTagKeyPress}
+            />
+          </div>
+          <div>
+            {this.state.tags.map(tag => {
+              return (
+                <span className="pt-tag pt-tag-removable" key={tag}>
+                  {tag}
+                  <button
+                    className="pt-tag-remove"
+                    type="button"
+                    onClick={this.removeTag.bind(null, tag)}
+                  />
+                </span>
+              );
+            })}
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="pt-button pt-intent-primary pt-icon-add buttonSpace"
+          onClick={this.handleSubmit}
+        >
+          Save Note
+        </button>
+      </div>
+    );
+  }
+}
+
+EntryInput.propTypes = {
+  onSubmit: PropTypes.func.isRequired
+};
+
+export default EntryInput;
--- client/src/react/log/Log.js
+++ client/src/react/log/Log.js
@@ -1,9 +1,111 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
+import moment from "moment";
+import MarkdownRenderer from "react-markdown-renderer";
+import apollo from "../../services/apolloServer";
+import EntryInput from "./EntryInput";
+import { NotifySuccess } from "../../services/notifier";
+import "./logs.css";
 
-class Log extends Component {
+function EntryDisplay(props) {
+  var createTime = moment(props.createdAt);
+  return (
+    <div className="note">
+      <span className="pt-icon-standard pt-icon-document" />
+      <div className="createdTime">{createTime.calendar()}</div>
+      <div className="content pt-card">
+        <MarkdownRenderer markdown={props.note} />
+      </div>
+      <div className="tags">
+        {props.tags &&
+          props.tags.map((t, i) => {
+            return (
+              <span key={i} className="pt-tag">
+                {t}
+              </span>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
+
+EntryDisplay.propTypes = {
+  createdAt: PropTypes.string.isRequired,
+  note: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired
+};
+
+function EntryListDisplay(props) {
+  return (
+    <div>
+      {" "}
+      {props.entries.map(function(entry, index) {
+        return (
+          <EntryDisplay
+            id={entry.id}
+            key={entry.id}
+            note={entry.note}
+            createdAt={entry.created_at}
+            tags={entry.tags}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+class Journal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      entries: []
+    };
+
+    this.loadEntries = this.loadEntries.bind(this);
+    this.addNewNote = this.addNewNote.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadEntries();
+  }
+
+  loadEntries() {
+    this.setState(() => {
+      return {
+        entries: []
+      };
+    });
+
+    apollo.invoke("GetAllJournalEntries", {}).then(data => {
+      this.setState(() => {
+        return {
+          entries: data
+        };
+      });
+    });
+  }
+
+  addNewNote(note, tags) {
+    apollo
+      .invoke("AddJournalEntry", {
+        note: note,
+        tags: tags
+      })
+      .then(() => {
+        NotifySuccess("Successfully added a new log entry.");
+        this.loadEntries();
+      });
+  }
+  log;
   render() {
-    return <div>Log</div>;
+    return (
+      <div>
+        <EntryInput onSubmit={this.addNewNote} />
+        <EntryListDisplay entries={this.state.entries} />
+      </div>
+    );
   }
 }
 
-export default Log;
+export default Journal;
