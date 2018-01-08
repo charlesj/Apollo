48f137ffc1c790ef589b54951550556dadc57ab4
Josh Charles <josh.charles@gmail.com>
2018-01-08 07:41:01 -0800

    Updating Application info display to display correct time

--- client/src/react/meta/ApplicationInfo.js
+++ client/src/react/meta/ApplicationInfo.js
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import moment from "moment";
 
-function ApplicationInfo(props) {
-  const { version, commitHash, compiledOn } = props.applicationInfo;
-  const compiledDisplay = moment(compiledOn).calendar();
+class ApplicationInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      compiledDisplay: null
+    };
+  }
 
-  return (
-    <div className="applicationInfo">
-      Apollo <strong>{version}</strong> ({commitHash && commitHash.slice(0, 6)})
-      - {compiledDisplay}
-    </div>
-  );
+  componentDidMount() {
+    var intervalId = setInterval(() => this.updateDisplay(), 1000);
+    this.setState({ intervalId: intervalId });
+  }
+
+  updateDisplay() {
+    if (this.props.applicationInfo) {
+      const { compiledOn } = this.props.applicationInfo;
+      if (compiledOn) {
+        this.setState({ compiledDisplay: moment(compiledOn).calendar() });
+      }
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  render() {
+    const { version, commitHash } = this.props.applicationInfo;
+    const { compiledDisplay } = this.state;
+
+    return (
+      <div className="applicationInfo">
+        Apollo <strong>{version}</strong> ({commitHash &&
+          commitHash.slice(0, 6)}) - {compiledDisplay}
+      </div>
+    );
+  }
 }
-
 function mapStateToProps(state, props) {
   const { applicationInfo } = state.meta;
 
