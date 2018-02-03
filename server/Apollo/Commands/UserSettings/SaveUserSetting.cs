73945269d65bb14101d64d8d4672c32491799784
Josh Charles <josh.charles@gmail.com>
2018-02-03 11:37:22 -0800

    Adding save user setting command

---
+++ server/Apollo/Commands/UserSettings/SaveUserSetting.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Apollo.CommandSystem;
+using Apollo.Data;
+using Apollo.Services;
+
+namespace Apollo.Commands.UserSettings
+{
+    public class SaveUserSetting : AuthenticatedCommand
+    {
+        private readonly IUserSettignsDataService dataService;
+
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public SaveUserSetting(ILoginService loginService, IUserSettignsDataService dataService) : base(loginService)
+        {
+            this.dataService = dataService;
+        }
+
+        public override async Task<CommandResult> Execute()
+        {
+            var setting = new UserSetting {name = Name, value = Value};
+            var updated = await dataService.UpdateSetting(setting);
+            return CommandResult.CreateSuccessResult(updated);
+        }
+
+        public override Task<bool> IsValid()
+        {
+            return Task.FromResult(!string.IsNullOrWhiteSpace(Name) && Value != null);
+        }
+
+        public override object ExamplePayload()
+        {
+            return new
+            {
+                name = string.Empty, value = string.Empty
+            };
+        }
+    }
+}
