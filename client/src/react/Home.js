50eb1bf3acd830c97e1191e3ca50e948cec337f3
Josh Charles <josh.charles@gmail.com>
2018-04-17 08:22:15 -0700

    Adding quick news links

--- client/src/react/Home.js
+++ client/src/react/Home.js
@@ -8,6 +8,18 @@ import FeaturedGoal from "./goals/FeaturedGoal";
 import Timeline from "./timeline/Timeline";
 
 function Home(props) {
+  const news = [
+    { link: "https://www.axios.com", label: "Axios" },
+    { link: "https://www.bloomberg.com/", label: "Bloomberg" },
+    { link: "https://www.theatlantic.com/", label: "The Atlantic" },
+    { link: "https://www.nytimes.com", label: "The New York Times" },
+    { link: "https://www.wsj.com/", label: "The Wall Street Journal" },
+    { link: "https://www.washingtonpost.com/", label: "The Washington Post" },
+    { link: "https://www.seattletimes.com/", label: "Seattle Times" },
+    { link: "https://www.foreignaffairs.com/", label: "Foreign Affairs" },
+    { link: "https://www.reuters.com/", label: "Reuters Wire" },
+
+  ]
   return (
     <div>
       <LifeCounter />
@@ -17,6 +29,13 @@ function Home(props) {
       <CompleteChecklist />
       <Strategies />
       <Timeline />
+      <div>
+        <ul>
+          { news.map(n => {
+            return <li key={n.label}><a href={n.link}>{n.label}</a></li>
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
